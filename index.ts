6184cd3274c20e3f20b4c009c11f9c2a953d6cb1
Tristan F. <LeoDog896@hotmail.com>
2024-10-09 12:20:47 -0700

    feat: full grabber

--- index.ts
+++ index.ts
@@ -1,45 +1,77 @@
-import { exists } from "jsr:@std/fs"
+import { exists } from "jsr:@std/fs";
 import { parse } from "jsr:@std/yaml";
 import { z } from "https://deno.land/x/zod@v3.23.8/mod.ts";
 
 // Clone oss-fuzz
 if (!(await exists("./output"))) {
-    const cmd = new Deno.Command("git", { args: ["clone", "--depth", "1", "https://github.com/google/oss-fuzz", "./output"] });
-    const { code } = await cmd.output();
+  const cmd = new Deno.Command("git", {
+    args: [
+      "clone",
+      "--depth",
+      "1",
+      "https://github.com/google/oss-fuzz",
+      "./output",
+    ],
+  });
+  const { code } = await cmd.output();
 
-    if (code !== 0) {
-        console.error("git clone failed.")
-        Deno.exit(1)
-    }
+  if (code !== 0) {
+    console.error("git clone failed.");
+    Deno.exit(1);
+  }
 }
 
 const projectSchema = z.object({
-    homepage: z.string().optional(),
-    main_repo: z.string().optional(),
-    language: z.string().optional()
-})
+  homepage: z.string().optional(),
+  main_repo: z.string().optional(),
+  language: z.string().optional(),
+});
 
-type Project = z.infer<typeof projectSchema>
+type Project = z.infer<typeof projectSchema>;
 
-const projects: Record<string, (Project | [error: unknown])> = {}
+const projects: Record<string, (Project | [error: unknown])> = {};
 
 // Go to projects, and acc projects
 for await (const dirEntry of Deno.readDir("./output/projects")) {
-    const projectYAML = await Deno.readTextFile(`./output/projects/${dirEntry.name}/project.yaml`);
+  const projectYAML = await Deno.readTextFile(
+    `./output/projects/${dirEntry.name}/project.yaml`,
+  );
 
-    try {
-        const parsedYAML = parse(projectYAML);
-        const sanitizedYAML = await projectSchema.safeParseAsync(parsedYAML)
-        
-        if (sanitizedYAML.error) {
-            console.error(`Error while getting metadata from ${dirEntry.name}:`);
-            projects[dirEntry.name] = [JSON.stringify(sanitizedYAML.error)];
-        }
+  try {
+    const parsedYAML = parse(projectYAML);
+    const sanitizedYAML = await projectSchema.safeParseAsync(parsedYAML);
 
-        projects[dirEntry.name] = sanitizedYAML.data!;
-    } catch (e) {
-        projects[dirEntry.name] = [JSON.stringify(e)];
+    if (sanitizedYAML.error) {
+      console.error(`Error while getting metadata from ${dirEntry.name}:`);
+      projects[dirEntry.name] = [JSON.stringify(sanitizedYAML.error)];
     }
+
+    projects[dirEntry.name] = sanitizedYAML.data!;
+  } catch (e) {
+    projects[dirEntry.name] = [JSON.stringify(e)];
+  }
 }
 
-await Deno.writeTextFile("./data.json", JSON.stringify(projects, null, 2))
+function markdownProject(name: string, project: Project | [error: unknown]) {
+  if (Array.isArray(project)) {
+    return `Couldn't parse. [View file](https://github.com/google/oss-fuzz/blob/master/projects/${name}/project.yaml).`;
+  }
+
+  return `([homepage](${project.homepage})) made in ${project.language}`;
+}
+
+await Deno.writeTextFile("./data.json", JSON.stringify(projects, null, 2));
+await Deno.writeTextFile(
+  "./README.md",
+  `# fuzz-directory
+
+sorted and usable list from [oss-fuzz](https://github.com/google/oss-fuzz/).
+
+## Projects
+
+${
+    Object.entries(projects).map(([name, project]) => {
+      return `- ${name}: ${markdownProject(name, project)}`;
+    }).join("\n")
+  }`,
+);
